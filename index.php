f8811a613679df09e9d2448865fe8ff07d6aef2c
mcginnis.loren <mcginnis.loren@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-01 21:09:51 +0000

    login preliminaries

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 #!/usr/local/bin/php
 <?
 # TODO
+require_once 'inc/util.inc';
+redir_if_logged_in('home.php');
+
 require_once 'inc/html_temp.inc';
 template_start_html();
 template_head('HRR');
--- login.php
+++ login.php
@@ -2,5 +2,15 @@
 <?
 # TODO
 # login user, set session variables, redirect to dashboard.
-header()
+include_once 'inc/util.inc';
+include_once 'inc/db_init.inc';
+redir_if_logged_in("logout.php");
+
+# check creditials using POST
+
+# set session
+set_logged_in('Test User',5);
+
+# send to dashboard
+redirect('home.php');
 ?>
