147dcae4f5ba79fa6bd9e2e558a7e7a776e57548
victorbendeck <victorbendeck@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-01 03:33:19 +0000

    updated mysql credentials

--- trunk/index.php
+++ trunk/index.php
@@ -1,13 +1,13 @@
 #!/usr/local/bin/php
 <?
-require_once("inc/include.inc");
-template_start_html();
-template_head("Homepage");
-template_start_body("A title");
+	require_once("inc/include.inc");
+	template_start_html();
+	template_head("Homepage");
+	template_start_body("A title");
 ?>
 <div>
     Here is some text.
 </div>
 <?
-template_end();
+	template_end();
 ?>
