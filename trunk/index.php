8f617c56ba7617ae6a381aaa1fb31c917d764c1a
mcginnis.loren <mcginnis.loren@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-01 19:56:44 +0000

    coded more php utilities added sql_scripts.inc file - empty queries to be written!

--- trunk/index.php
+++ trunk/index.php
@@ -1,13 +1,13 @@
 #!/usr/local/bin/php
 <?
-require_once("inc/include.inc");
+require_once 'inc/html_temp.inc';
 template_start_html();
-template_head("Homepage");
-template_start_body("A title");
+template_head('HRR');
+template_start_body('Harvard Room Reservation');
 ?>
 <div>
     Here is some text.
 </div>
 <?
-	template_end();
+template_end();
 ?>
