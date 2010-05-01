a741adbde6245e7330dd06b1e10c1bb7c9614510
victorbendeck <victorbendeck@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-01 18:11:41 +0000

    test data and ddl1.1

--- trunk/sql/ddl.sql
+++ trunk/sql/ddl.sql
@@ -6,6 +6,7 @@ read_events BOOL NOT NULL,
 write_events BOOL NOT NULL
 ) ENGINE=InnoDB;
 
+
 CREATE TABLE users(
 huid CHAR(8) NOT NULL PRIMARY KEY,
 pwd VARCHAR(40) NOT NULL,
@@ -18,28 +19,23 @@ FOREIGN KEY (role) REFERENCES priv(role)
 ) ENGINE=InnoDB;
 
 
-INSERT INTO priv VALUES 
-('f','faculty',10,true,false),
-('t','tf',8,true,false),
-('s','student_grp',5,false,false),
-('b','building_mgr',12,true,true),
-('a','admin_asst',9,true,false),
-('r','sysadmin',15,true,true); 
-
 CREATE TABLE campus(
 id CHAR(1) NOT NULL PRIMARY KEY,
 name VARCHAR(30) NOT NULL
 ) ENGINE=InnoDB;
 
+
 CREATE TABLE building(
-id VARCHAR(10) NOT NULL PRIMARY KEY,
+id CHAR(2) NOT NULL PRIMARY KEY,
 region CHAR(1) NOT NULL,
+name VARCHAR(30) NOT NULL,
 FOREIGN KEY (region) REFERENCES campus(id)
 ) ENGINE=InnoDB;
 
+
 CREATE TABLE room(
 room_num VARCHAR(6) NOT NULL,
-building CHAR(10) NOT NULL,
+building CHAR(2) NOT NULL,
 projector BOOL NOT NULL,
 capacity INT NOT NULL,
 PRIMARY KEY (room_num,building),
@@ -48,9 +44,10 @@ FOREIGN KEY (building) REFERENCES building(id)
 
 
 CREATE TABLE events(
+event_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 huid CHAR(8) NOT NULL,
 room VARCHAR(6) NOT NULL,
-building CHAR(10) NOT NULL,
+building CHAR(2) NOT NULL,
 date_req DATETIME NOT NULL,
 duration INT NOT NULL,
 description VARCHAR(60) NOT NULL,
@@ -59,5 +56,6 @@ recurring VARCHAR(20),
 FOREIGN KEY (room,building) REFERENCES room(room_num,building),
 FOREIGN KEY (huid) REFERENCES users(huid)
 ) ENGINE=InnoDB;
+
 
 
