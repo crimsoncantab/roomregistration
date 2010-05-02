faa9a5a39d8c37b813f612d4e03be26fcc0b3137
victorbendeck <victorbendeck@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-02 08:03:29 +0000

    formatting2

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 <?
-# TODO
-# login user, set session variables, redirect to dashboard.
+	# login user, set session variables, redirect to dashboard.
 	include_once 'inc/util.inc';
 	include_once 'inc/db_init.inc';
 
 	# check credentials using POST
-	authuser($_POST['uid'], $_POST['pwd']);
+	authUser($_POST['uid'], $_POST['pwd']);
 	redirect('home.php');
 
 ?>
