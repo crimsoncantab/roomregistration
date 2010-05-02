64d5e2a98308a993db8a66e2b6586f1bb70e6d93
mcginnis.loren <mcginnis.loren@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-02 03:40:07 +0000

    login preliminaries

--- trunk/login.php
+++ trunk/login.php
@@ -3,12 +3,11 @@
 # login user, set session variables, redirect to dashboard.
 include_once 'inc/util.inc';
 include_once 'inc/db_init.inc';
-redir_if_logged_in("logout.php");
 
 # check creditials using POST
 
 # set session
-set_logged_in('Test User','5');
+//set_logged_in($uid, $username);
 
 # send to dashboard
 redirect('home.php');
