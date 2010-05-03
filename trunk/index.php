67d6d02d414c4fbd56b8500453980f3f94b816f6
mcginnis.loren <mcginnis.loren@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-03 03:26:04 +0000

    logout now calls session_destroy() reconfigured how pages were autheticated against user, using roles added table to handle page authentication, populated with data implemented links sidebar using same methods added queries for page auth removed link identifier param from mysql_safe the role char for no user is '0'

--- trunk/index.php
+++ trunk/index.php
@@ -1,15 +1,10 @@
 <?
-	require_once 'inc/util.inc';
-	redir_if_logged_in('home.php');
-
 	require_once 'inc/html_temp.inc';
-	template_start_html();
-	template_head('HRR');
-	template_start_body('Harvard Room Reservation');
+	$role = get_user_perm();
+        template_start(basename(__FILE__), 'HRR', 'Harvard Room Reservation', 'home.php');
 ?>
 
 <div>
-	<? print_r($_SESSION) ?>
     Welcome to the room reservation tool. Please login above.
 </div>
 
--- trunk/logout.php
+++ trunk/logout.php
@@ -2,6 +2,6 @@
 	#logout user
 	require_once 'inc/util.inc';
 	//unset($_SESSION['uid']);
-	$_SESSION=array();
+	session_destroy();
 	redirect(".");
 ?>
