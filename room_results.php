e4daefba436c80b425a6966baca159dc35d2991c
victorbendeck <victorbendeck@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-03 20:01:39 +0000

    only campus search is needed now

--- room_results.php
+++ room_results.php
@@ -11,7 +11,34 @@
 		echo "no campus";
 	else
 		echo "campus selected";
+	if($_GET['campus']==""&&$_GET['building']=="")
+		$result=getRooms($_GET['capacity'],$_GET['projector']);
+	else
+	{
+		if($_GET['building']!="")
+			$result=getBuildingRooms($_GET['building'],$_GET['capacity'],$_GET['projector']);		
+		else if($_GET['campus']!="")
+			$result=getCampusRooms($_GET['campus'],$_GET['capacity'],$_GET['projector']);
+	}
 
+	echo "<table border='1'><tr>";
+	for($i = 0; $i < mysql_num_fields($result); $i++)
+	{
+		echo "<th>".mysql_field_name($result, $i)."</th>";
+	}
+	echo "</tr>";
+	
+	while($row = mysql_fetch_array($result))
+	{
+		echo "<tr>";
+		for($i = 0; $i < mysql_num_fields($result); $i++)
+		{
+			echo "<td>". $row[$i] ."</td>";
+		}
+		echo "</tr>";
+	}
+
+	echo "</table>";
 ?>
 </div>
 
