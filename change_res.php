cdee84dcf4af622fde34ee52de1b8129f43101e2
mcginnis.loren <mcginnis.loren@6e825d2a-75a8-c81d-e248-4deaa321afd8>
2010-05-06 15:55:08 +0000

    modifying an event is now easier

--- change_res.php
+++ change_res.php
@@ -18,38 +18,56 @@ if ($update) $event = getEvent($_GET['event_id']);
             <?
             Event::getEventHeaderRow();
             $event->getHtmlRow(false);
+            $parsed_start = getdate($event->getStart());
+            $parsed_end = getdate($event->getEnd());
             ?>
     </table>
         <?
     }
     ?>
     <form action="handle_req.php" method="post" id="change_form">
-<?
-if ($update) echo '<input type="hidden" name="event_id" value="'.$_GET['event_id'].'" />';
-if (!($update && $event->isRecurring())) {
-?>
+        <?
+        if ($update) echo '<input type="hidden" name="event_id" value="'.$_GET['event_id'].'" />';
+        if (!($update && $event->isRecurring())) {
+            ?>
         Month:
-        <?template_month_dropdown();?>
-        Day:
-        <select name="day">
-            <option selected></option>
+        ?>
+        <select name="month">
+            <?if (!$update) {?><option selected></option><?}?>
+                <?
+                for($i=1;$i<=12;$i++) {
+                    echo '<option value="'.$i.'"';
+                    if ($update && $i==$parsed_start['month']) echo ' selected';
+                    echo '>';
+                    echo date('M',mktime(0, 0, 0, $i));
+                    echo '</option>';
+                }
+                ?>
+        </select>
+            Day:
+            <select name="day">
+            <?if (!$update) {?><option selected></option><?}?>
             <?
             for($i=1;$i<=31;$i++) {
-                echo '<option value="'.$i.'">';
+                echo '<option value="'.$i.'"';
+                if ($update && $i==$parsed_start['mday']) echo ' selected';
+                echo '>';
                 echo $i;
                 echo '</option>';
             }
             ?>
         </select>
-<?
-}
-?>
+            <?
+        }
+        ?>
         Priority:
         <select name="priority">
-            <option selected></option>
+            <?if (!$update) {?><option selected></option><?}?>
             <?
             for($i=1;$i<=$_SESSION['max_priority'];$i++) {
-                echo '<option value="'.$i.'">';
+                echo '<option value="'.$i.'"';
+                if ($update && $i == $event->getPriority()) echo ' selected';
+                echo '>';
                 echo $i;
                 echo '</option>';
             }
@@ -58,32 +76,30 @@ if (!($update && $event->isRecurring())) {
         <br />
         Room:
         <select name="roomselect" onchange="javascript:set_vals(this);">
-            <option selected></option>
+            <?if (!$update) {?><option selected></option><?}?>
             <?
             $results = getAllRooms();
             while ($row = mysql_fetch_array($results)) {
-                echo '<option value="'.$row['room_num'].'$'.$row['building'].'">'.$row['building'].' '.$row['room_num'].'</option>';
+                echo '<option value="'.$row['room_num'].'$'.$row['building'].'"';
+                if ($update && $row['room_num'] == $event->getRoom() &&$row['building'] == $event->getBldg()) echo ' selected';
+                echo '>'.$row['building'].' '.$row['room_num'].'</option>';
             }
             ?>
         </select>
-        <input type="hidden" name="room" value="" />
-        <input type="hidden" name="building" value="" />
+        <input type="hidden" name="room" value="<?if ($update) echo $event->getRoom();?>" />
+        <input type="hidden" name="building" value="<?if ($update) echo $event->getBldg();?>" />
         <br />
-        <?
-        if (!$update) {
-            ?>
         Description:
-        <input type="text" name="description" value="" />
+        <input type="text" name="description" value="<?if ($update) echo $event->getDesc();?>" />
         <br />
-            <?
-        }
-        ?>
         Start time:
         <select name="start_time">
-            <option selected></option>
+            <?if (!$update) {?><option selected></option><?}?>
             <?
             for($i=$GLOBALS['beg'];$i<=$GLOBALS['end'];$i+=0.5) {
-                echo "<option>";
+                echo '<option value="'.$i.'"';
+                if ($update && (((int)$i)==$parsed_start['hours'])&& (($i - (int)$i)*60==$parsed_start['minutes'])) echo ' selected';
+                echo '>';
                 echo $i;
                 echo "</option>";
             }
@@ -91,19 +107,21 @@ if (!($update && $event->isRecurring())) {
         </select>
         End time:
         <select name="end_time">
-            <option selected></option>
+            <?if (!$update) {?><option selected></option><?}?>
             <?
             for($i=$GLOBALS['beg'];$i<=$GLOBALS['end'];$i+=0.5) {
-                echo "<option>";
+                echo '<option value="'.$i.'"';
+                if ($update && (((int)$i)==$parsed_end['hours'])&& (($i - (int)$i)*60==$parsed_end['minutes'])) echo ' selected';
+                echo '>';
                 echo $i;
                 echo "</option>";
             }
             ?>
         </select>
         <br />
-<?
-if (!$update) {
-?>
+        <?
+        if (!$update) {
+            ?>
         Recurrence:
         S<input type="checkbox" name="recurring[]" value="1" />
         |M<input type="checkbox" name="recurring[]" value="2" />
@@ -112,9 +130,9 @@ if (!$update) {
         |Th<input type="checkbox" name="recurring[]" value="5" />
         |F<input type="checkbox" name="recurring[]" value="6" />
         |Sa<input type="checkbox" name="recurring[]" value="7" />
-<?
-}
-?>
+            <?
+        }
+        ?>
         <br />
         <input type="submit" value="Submit" name="submit" />
     </form>
